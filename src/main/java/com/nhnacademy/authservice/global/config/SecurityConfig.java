cf17773aca06acd1922d8993a001455fdf268cba
chosun-nhn18 <kimshh21@gmail.com>
2025-12-27 18:53:30 +0900

    feat: SecurityConfig Stateless 설정 및 쿠키 기반 OAuth2 저장소 구현
    - 세션 대신 쿠키를 사용하여 OAuth2 인증 요청 정보를 저장하도록 변경
    - HttpCookieOAuth2AuthorizationRequestRepository, CookieUtils 추가

--- src/main/java/com/nhnacademy/authservice/global/config/SecurityConfig.java
+++ src/main/java/com/nhnacademy/authservice/global/config/SecurityConfig.java
@@ -1,17 +1,16 @@
 package com.nhnacademy.authservice.global.config;
 
 import com.nhnacademy.authservice.auth.jwt.SocialLoginHandler;
+import com.nhnacademy.authservice.auth.oauth2.HttpCookieOAuth2AuthorizationRequestRepository;
 import com.nhnacademy.authservice.auth.service.CustomOAuth2UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.core.StringRedisTemplate;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -21,39 +20,32 @@ public class SecurityConfig {
 
     private final SocialLoginHandler socialLoginHandler;
     private final CustomOAuth2UserService customOAuth2UserService;
-    private final StringRedisTemplate redisTemplate;
+    private final HttpCookieOAuth2AuthorizationRequestRepository cookieAuthorizationRequestRepository;
 
     @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) {
-        return configuration.getAuthenticationManager();
-    }
-
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) {
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception { // Exception 추가 필요
 
         http.csrf(AbstractHttpConfigurer::disable)
                 .formLogin(AbstractHttpConfigurer::disable)
                 .httpBasic(AbstractHttpConfigurer::disable)
                 .logout(AbstractHttpConfigurer::disable);
 
-        // 프레임 옵션 설정: 모든 URL의 프레임 보호 해제(X-Frame-Options를 비활성화)
-        // h2 DB를 위해 dev 환경에서만 X-Frame-Options 임시 비활성화
         http.headers((headers) -> headers
                 .frameOptions(HeadersConfigurer.FrameOptionsConfig::disable));
 
-        // OAuth2 설정
+        http.sessionManagement((session) -> session
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
         http.oauth2Login((oauth2) -> oauth2
-                        .userInfoEndpoint((userInfo) -> userInfo
-                                .userService(customOAuth2UserService))
-                        .successHandler(socialLoginHandler)
+                // 인증 요청을 쿠키에 저장하도록 설정
+                .authorizationEndpoint(authorization -> authorization
+                        .baseUri("/oauth2/authorization")
+                        .authorizationRequestRepository(cookieAuthorizationRequestRepository))
+                .userInfoEndpoint((userInfo) -> userInfo
+                        .userService(customOAuth2UserService))
+                .successHandler(socialLoginHandler)
         );
 
-        // 소셜 로그인 문제로 주석 처리
-        // OAuth2 로그인 과정에서 임시 세션 필요
-//        http.sessionManagement((session) -> session
-//                .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-
-        // 모든 요청 허용
         http.authorizeHttpRequests(auth ->
                 auth.anyRequest().permitAll()
         );
