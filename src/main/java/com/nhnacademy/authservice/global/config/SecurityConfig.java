8c250e5330f6601a720dd7b124f5e491610f3ce0
chosun-nhn18 <kimshh21@gmail.com>
2025-12-27 19:25:49 +0900

    fix: authenticationManager Bean 등록

--- src/main/java/com/nhnacademy/authservice/global/config/SecurityConfig.java
+++ src/main/java/com/nhnacademy/authservice/global/config/SecurityConfig.java
@@ -6,6 +6,8 @@ import com.nhnacademy.authservice.auth.service.CustomOAuth2UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -21,6 +23,11 @@ public class SecurityConfig {
     private final SocialLoginHandler socialLoginHandler;
     private final CustomOAuth2UserService customOAuth2UserService;
     private final HttpCookieOAuth2AuthorizationRequestRepository cookieAuthorizationRequestRepository;
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception { // Exception 추가 필요
